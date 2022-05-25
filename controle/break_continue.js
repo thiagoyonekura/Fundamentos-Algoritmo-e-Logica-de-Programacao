33592ca1fe3cda47845edaed66e98e837a81ac70
thiagoyonekura <thiagoyonekura@gmail.com>
2022-05-25 00:25:42 -0300

    aulas

---
+++ controle/break_continue.js
@@ -0,0 +1,15 @@
+//break
+for(let i = 1; i <= 5; i++) {
+    if(i === 3) {
+         break; //TERMINA O LAÇO
+    }
+    console.log(i);
+}
+
+//continue
+for(let i = 1; i <= 5; i++) {
+    if(i === 3) {
+        continue; // CONTINUA
+    }
+    console.log(i);
+}
