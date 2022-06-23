be3d0ca953d15162f8095225cc5587cb9d8b11be
thiagoyonekura <thiagoyonekura@gmail.com>
2022-06-22 21:30:27 -0300

    fundamentos

---
+++ funcoes/com_funcao.js
@@ -0,0 +1,24 @@
+const d1 = 3;
+const m1 = 7;
+const a1 = 2021;
+
+function exibirData1() {
+    console.log(`Dia: ${d1}/${m1}/${a1}`);
+}
+
+const d2 = 23;
+const m2 = 12;
+const a2 = 2021;
+
+function exibirData2() {
+    console.log(`Dia: ${d2}-${m2}-${a2}`);
+
+}
+
+exibirData1();
+exibirData1();
+exibirData1();
+
+exibirData2();
+exibirData2();
+exibirData2();
