8b87dfd11ba0a0e7f14904e5d886251a3bee4686
thiagoyonekura <thiagoyonekura@gmail.com>
2022-07-12 10:04:10 -0300

    fundamentos

--- objeto/basico_2.js
+++ objeto/basico_2.js
@@ -1,9 +1,9 @@
 const produto = {
     nome: 'Ipad',
     preco: 5600,
-    desconto: 0.15,
-    precoComDesconto: function(){
-        return preco * (1 - desconto);
+    desconto: 0.5,
+    precoComDesconto: function () {
+        return this.preco * (1 - this.desconto);
     }
 };
 
